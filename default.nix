eb07dba4e79865f426c51e8caea2339436e1cb63
Tilman Holube <tilman@holube.de>
2024-01-02 12:51:41 +0100

    stdenv

--- default.nix
+++ default.nix
@@ -3,6 +3,7 @@
 stdenv.mkDerivation {
   name = "til7701";
   inherit buildInputs;
+  inherit stenv;
 
   modules = import ./modules { inherit lib; };
   packages = import ./packages { inherit lib; };

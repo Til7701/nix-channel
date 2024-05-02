a91f435df0e28456d0c03e350c8b4d209f4d9af3
Tilman Holube <45685205+Til7701@users.noreply.github.com>
2024-05-03 01:28:43 +0200

    Create hyfetch.nix

---
+++ packages/hyfetch.nix
@@ -0,0 +1,46 @@
+# copied from nixpkgs to use fork
+{ lib
+, fetchFromGitHub
+, python3
+}:
+
+python3.pkgs.buildPythonApplication rec {
+  pname = "hyfetch";
+  version = "1.4.11";
+  format = "setuptools";
+
+  src = fetchFromGitHub {
+    owner = "Til7701";
+    repo = "hyfetch";
+    rev = "refs/tags/test";
+    hash = "";
+  };
+
+  propagatedBuildInputs = with python3.pkgs; [
+    typing-extensions
+    setuptools
+  ];
+
+  # No test available
+  doCheck = false;
+
+  pythonImportsCheck = [
+    "hyfetch"
+  ];
+
+  meta = with lib; {
+    description = "neofetch with pride flags <3";
+    longDescription = ''
+      HyFetch is a command-line system information tool fork of neofetch.
+      HyFetch displays information about your system next to your OS logo
+      in ASCII representation. The ASCII representation is then colored in
+      the pattern of the pride flag of your choice. The main purpose of
+      HyFetch is to be used in screenshots to show other users what
+      operating system or distribution you are running, what theme or
+      icon set you are using, etc.
+    '';
+    homepage = "https://github.com/Til7701/HyFetch";
+    license = licenses.mit;
+    mainProgram = "hyfetch";
+  };
+}
